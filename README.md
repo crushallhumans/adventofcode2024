7173ee197a8ac3a170d2cacc7dfc12ab3589ee43
Conrad Rushing <conrad.rushing@gmail.com>
2024-12-01 14:26:46 -0500

    Frist poast

---
+++ README.md
@@ -0,0 +1,6 @@
+# adventofcode2023
+Advent of Code 2023	
+
+Test functions: python3 -m unittest day01.py  
+Get solutions:  python3 day01.py  
+Print puzzle:   python3 day01.py foo  
---
+++ _template.py
@@ -0,0 +1,117 @@
+# adventofcode 2023
+# crushallhumans
+# puzzle N
+# 12/n/2023
+
+import os
+import re
+import sys
+import math
+import unittest
+import socket
+import hashlib
+import pprint
+import random
+import time
+from functools import reduce
+from functools import cache
+from itertools import chain
+from multiprocessing import Pool
+pp = pprint.PrettyPrinter()
+
+ADVENT_YEAR = '2024'
+DEBUG = False
+
+def one_star(param_set, is_two_star = False):
+    print("---------------one_star--------------------")
+    param_set = reprocess_input(param_set)
+    c = 8888
+    for i in param_set:
+        continue
+    return c
+
+
+def two_star(param_set):
+    print("---------------two_star--------------------")
+    param_set = reprocess_input(param_set)
+    c = 7777
+    for i in param_set:
+        continue
+    return c
+
+def reprocess_input(param_set):
+    if isinstance(param_set,str):
+        l = []
+        l = [input_line.strip() for input_line in param_set.splitlines()]
+        param_set = l
+    return param_set    
+
+
+def puzzle_text():
+    print("""--- Day N: X ---
+
+""")
+
+
+
+class testCase(unittest.TestCase):
+    global DEBUG
+    DEBUG = True
+
+    test_set = (
+        """
+"""
+    )
+
+    def test_one_star(self):
+        self.assertEqual(
+            one_star(
+                self.__class__.test_set
+            ),
+            8888
+        )
+
+    def test_two_star(self):
+        self.assertEqual(
+            two_star(
+                self.__class__.test_set
+            ),
+            7777
+        )
+
+
+
+if __name__ == '__main__':
+    try:
+        sys.argv[1]
+        puzzle_text()
+
+    except:
+        DEBUG = False
+
+        username = 'crushing'
+        m = hashlib.sha256()
+        hostname = socket.gethostname()
+        m.update(hostname.encode('utf8'))
+        if m.hexdigest() == 'ec7c98e2b47378ec88e1f9cce8d6ed91b9d616787c8a37023fd5c67cef1ff71f':
+            username = 'conrad.rushing'
+        print ('hostname str :',hostname)
+        print ('hostname hash:', m.hexdigest())
+
+        filename_script = os.path.basename(__file__)
+        print("---------------%s--------------------"%filename_script)
+        filename = filename_script.split('.')[0]
+        input_set = ()
+        
+        with open("/Users/%s/Development/crushallhumans/adventofcode%s/inputs/%s.txt" % (username,ADVENT_YEAR,filename)) as input_file:
+            input_set = [input_line.strip() for input_line in input_file]
+
+        start = (time.time() * 1000)
+        ret = one_star(input_set)
+        print (ret)
+        print ('elapsed:',(time.time() * 1000) - start,'ms')
+
+        start = (time.time() * 1000)
+        ret = two_star(input_set)
+        print (ret)
+        print ('elapsed:',(time.time() * 1000) - start,'ms')
